cf20962a91588c4895af486ba45f3df1d99b208e
Tyler Grinn <tylergrinn@gmail.com>
2018-01-19 13:06:13 -0500

    stuff

--- src/node-scripts/copy-config.ts
+++ src/node-scripts/copy-config.ts
@@ -4,8 +4,6 @@ import * as fs from 'fs';
 
 const arg = process.argv[2];
 
-console.error("CWD: " + process.cwd());
-
 const packageConfigPath = path.join(process.cwd(), arg);
 const configName = arg.split('/').slice(-1)[0];
 const localConfigPath = path.join(process.cwd(), '../../../configs');
